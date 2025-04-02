104919cee24ba421b9039dd17fdf135a34f35ee3
mihirpa18 <punchithayamihir@gmail.com>
2025-04-02 16:35:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int n=s.length();
+        int nxt=numRows+(numRows-2);
+        string res;
+        int j=0;
+        if(numRows==n){
+            return s;
+        }
+        for(int i=0;i<numRows;i++){
+            int tempNxt=nxt-i;
+            j=i;
+            while(j<n ){
+                if(nxt==tempNxt){
+                    res.push_back(s[j]);
+                    j=j+nxt;
+                }
+                else{
+                    res.push_back(s[j]);
+                    if(tempNxt<n && tempNxt!=i){
+                        res.push_back(s[tempNxt]);
+                        tempNxt=tempNxt+nxt;
+                    }
+                    j=j+nxt;
+                }
+            }
+        }
+
+        return res;
+    }
+};
