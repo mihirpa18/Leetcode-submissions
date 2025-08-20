2c6c79da7d58054a1e27d0b756e5758b0d6ac5b3
mihirpa18 <punchithayamihir@gmail.com>
2025-08-20 15:54:09 +0530

    Time: 3 ms (82.96%), Space: 30.1 MB (82.73%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        //look for rightmost ending sqr that is squares whoes right end is (i,j)
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0) continue;
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                    count+=dp[i][j];
+                }
+                else {
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+                    count+=dp[i][j];
+                }
+            }
+        }
+
+        return count;
+    }
+};
