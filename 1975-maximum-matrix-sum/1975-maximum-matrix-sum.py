d31ca9b76900cac6277d591ccd5f5557ca8a99f3
mihirpa18 <punchithayamihir@gmail.com>
2026-01-05 10:53:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxMatrixSum(self, mat: List[List[int]]) -> int:
+        n=len(mat)
+        totSum=0
+        neg=0
+        minAbsVal=float('inf')
+        for i in range(n):
+            for j in range(n):
+                totSum+=abs(mat[i][j])
+                if(mat[i][j]<0):
+                    neg+=1
+                    minAbsVal=min(minAbsVal,abs(mat[i][j]))
+        # print(totSum)
+        # print(neg)
+                
+        if(neg%2!=0):
+            totSum-=2*minAbsVal
+
+        return totSum
