9ed1e69bf50e78eb0c26b2e464055234af992816
mihirpa18 <punchithayamihir@gmail.com>
2026-03-14 11:08:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.34%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,27 +1,60 @@
 class Solution {
-    void generate(string s,int i,int n,vector<string>&arr){
-        cout<<s<<" ";
-        if(i==n){
-            arr.push_back(s);
-            return;
-        }
-        if(i==0 || s==""){
-            generate(s+'a',i+1,n,arr);
-            generate(s+'b',i+1,n,arr);
-            generate(s+'c',i+1,n,arr);
-        }
-        else{
-            if(s[i-1]!='a') generate(s+'a',i+1,n,arr);
-            if(s[i-1]!='b') generate(s+'b',i+1,n,arr);
-            if(s[i-1]!='c') generate(s+'c',i+1,n,arr);
-        }
-    }
+    // void generate(string s,int i,int n,vector<string>&arr){
+    //     cout<<s<<" ";
+    //     if(i==n){
+    //         arr.push_back(s);
+    //         return;
+    //     }
+    //     if(i==0 || s==""){
+    //         generate(s+'a',i+1,n,arr);
+    //         generate(s+'b',i+1,n,arr);
+    //         generate(s+'c',i+1,n,arr);
+    //     }
+    //     else{
+    //         if(s[i-1]!='a') generate(s+'a',i+1,n,arr);
+    //         if(s[i-1]!='b') generate(s+'b',i+1,n,arr);
+    //         if(s[i-1]!='c') generate(s+'c',i+1,n,arr);
+    //     }
+    // }
 public:
     string getHappyString(int n, int k) {
-        vector<string>arr;
-        generate("",0,n,arr);
-        sort(arr.begin(),arr.end());
-        return arr.size()>=k?arr[k-1]:"";
+        int totalStrings = 3 * (1<<(n-1));  //3*2^(n-1)
+        if(k > totalStrings) return "";
+        
+        //each string can be divided in 3 blocks (starting with a,b,and c) so totalString/3
+        int blockSize = 1<<(n-1);
+
+        string res = "";
+        k--;  //0 indexed
+
+        int firstCharIdx = k/blockSize;    //finding first char -> since divided into blocks
+        res+=(char)('a' + firstCharIdx);  
+
+        k= k % blockSize;  //how much th item in block 
+
+        for(int i = 1; i<n ;i++){
+            blockSize = blockSize >> 1;     //halve the blockSize
+            int charIdx = k/blockSize;   
+
+            char prev = res.back();
+
+            if(prev == 'a'){
+                if(charIdx == 0) res += 'b';
+                else res += 'c';
+            }
+            else if(prev == 'b'){
+                if(charIdx == 0) res += 'a';
+                else res += 'c';
+            }
+            else{
+                if(charIdx == 0) res += 'a';
+                else res += 'b';
+            }
+            
+            k %= blockSize;   //update k for next iteration
+        }
+
+        return res;
 
     }
 };
